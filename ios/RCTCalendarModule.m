e3187f96cc75e83d052621977f14a4893d81b315
Lizzi Lindboe <lindboe@users.noreply.github.com>
2022-04-19 16:32:25 -0700

    use callback

--- ios/RCTCalendarModule.m
+++ ios/RCTCalendarModule.m
@@ -14,12 +14,19 @@
 @implementation RCTCalendarModule
 
 // RCT_EXPORT_METHOD macro will not be necessary with TM unless the method relies on RCT argument conversion; should avoid RCTConvert as macros will be discontinued
-RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name
+                  location:(NSString *)location
+                  myCallback:(RCTResponseSenderBlock) callback)
 {
-   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+  NSInteger eventId = 1;
+  callback(@[name, location, @(eventId)]);
 }
 
 // To export a module named RCTCalendarModule
 RCT_EXPORT_MODULE();
+
+// Notes
+// constantsToExport is even more complicated on iOS
+// iOS doesn't have the note about TM making it so that constants are fetched every invocation?
 
 @end
