2c3c0f4d081ded591710f53ef6b353e546f41585
Lizzi Lindboe <lindboe@users.noreply.github.com>
2022-05-02 15:19:52 -0700

    Sample module stuff

--- App.tsx
+++ App.tsx
@@ -1,112 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-import type {Node} from 'react';
+import {useState} from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+SafeAreaView,
+StatusBar,
+Text,
+Button,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+import Accelerometer from 'accel-lib/src/index';
+const App =  () => {
+const [currentResult, setResult] = useState<number | null>(null);
+return (
+    <SafeAreaView>
+    <StatusBar barStyle={'dark-content'}/>
+    <Text style={{marginLeft:20, marginTop:20}}>3+7={currentResult ?? "??"}</Text>
+    <Button title="Compute" onPress={async () => {
+        const result = await Accelerometer.add(3, 7);
+        setResult(result);
+    }} />
     </SafeAreaView>
-  );
+);
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+// import React, {useEffect, useReducer, useRef, useState} from 'react';
+// 
+// import {
+//   SafeAreaView,
+//   NativeModules,
+//   Button,
+//   ScrollView,
+//   Image,
+//   Text,
+//   NativeEventEmitter,
+//   EmitterSubscription,
+// } from 'react-native';
+// 
+// function AccelerometerObserver() {
+//   const [shouldObserve, setShouldObserve] = useState(true);
+//   const eventListener = useRef<EmitterSubscription | undefined>(undefined);
+//   const xData = useRef({lastX: undefined, xChanged: false});
+//   const [direction, setDirection] = useState(undefined);
+//   // instead of changed/not changed, try moved left/moved right
+// 
+//   useEffect(() => {
+//     if (shouldObserve) {
+//       console.log('STARTING');
+//       const eventEmitter = new NativeEventEmitter(NativeModules.Accelerometer);
+//       eventListener.current = eventEmitter.addListener(
+//         'Accelerometer',
+//         event => {
+//           console.log(event);
+//           const {lastX} = xData.current;
+//           if (lastX === undefined || lastX === event.x) {
+//             xData.current = {lastX: event.x, xChanged: true};
+//           } else if (event.x < lastX) {
+//             console.log('LEFT');
+//             xData.current = {lastX: event.x, xChanged: true};
+//             setDirection('left');
+//           } else if (event.x > lastX) {
+//             xData.current = {lastX: event.x, xChanged: false};
+//             setDirection('right');
+//           }
+//         },
+//       );
+//       return () => eventListener.current?.remove();
+//     } else if (!shouldObserve) {
+//       eventListener.current?.remove();
+//     }
+//   }, [shouldObserve]);
+// 
+//   return (
+//     <>
+//       <Button title="start events" onPress={() => setShouldObserve(true)} />
+//       <Button title="stop events" onPress={() => setShouldObserve(false)} />
+//       <Text>X Changing: {direction}</Text>
+//     </>
+//   );
+// }
+// 
+// const App = () => {
+//   return (
+//     <SafeAreaView>
+//       <AccelerometerObserver />
+//       <Text>Old Architecture</Text>
+//     </SafeAreaView>
+//   );
+// };
+// 
+// export default App;
