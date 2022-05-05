fae8025a61bd4955bf0ce09a4a47941f488b73e9
Lizzi Lindboe <lindboe@users.noreply.github.com>
2022-05-05 15:50:36 -0700

    Use demo components

--- App.tsx
+++ App.tsx
@@ -1,57 +1,16 @@
 
-import React, {useEffect, useReducer, useRef, useState} from 'react';
-import Accelerometer from 'accel-lib/src/index';
+import React from 'react';
 
-import {
-  SafeAreaView,
-  Button,
-  Text,
-  NativeEventEmitter,
-  EmitterSubscription,
-} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import Demo from 'accel-lib/src/index';
 
-function AccelerometerObserver() {
-  const [shouldObserve, setShouldObserve] = useState(true);
-  const eventListener = useRef<EmitterSubscription | undefined>(undefined);
-  const xData = useRef({lastX: undefined, xChanged: false});
-  const [direction, setDirection] = useState(undefined);
-  // instead of changed/not changed, try moved left/moved right
-
-  useEffect(() => {
-    if (shouldObserve) {
-      console.log('STARTING');
-      const eventEmitter = new NativeEventEmitter(Accelerometer);
-      eventListener.current = eventEmitter.addListener(
-        'Accelerometer',
-        event => {
-          console.log(event);
-          if (event.x === 0) {
-            setDirection('Not moving')
-          } else {
-            setDirection('moving')
-          }
-        },
-      );
-      return () => eventListener.current?.remove();
-    } else if (!shouldObserve) {
-      eventListener.current?.remove();
-    }
-  }, [shouldObserve]);
-
-  return (
-    <>
-      <Button title="start events" onPress={() => setShouldObserve(true)} />
-      <Button title="stop events" onPress={() => setShouldObserve(false)} />
-      <Text>X Changing: {direction}</Text>
-    </>
-  );
-}
+const {Stateful} = Demo;
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <AccelerometerObserver />
-      <Text>New Architecture</Text>
+    <SafeAreaView style={{height: '100%', width: '100%'}}>
+      <Stateful />
+      <Text>New Architecture, 0.69.0-rc0</Text>
     </SafeAreaView>
   );
 };
