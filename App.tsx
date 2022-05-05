091032210480b42048621a870637dab509f8aa0d
Lizzi Lindboe <lindboe@users.noreply.github.com>
2022-05-05 15:20:52 -0700

    Use new demo component

--- App.tsx
+++ App.tsx
@@ -1,58 +1,17 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 
-import {
-  SafeAreaView,
-  Button,
-  Text,
-  NativeEventEmitter,
-  EmitterSubscription,
-} from 'react-native';
-import Accelerometer from 'accel-lib/src/index';
+import {SafeAreaView, Text} from 'react-native';
+import Demo from 'accel-lib/src/index';
 
-function AccelerometerObserver() {
-  const [shouldObserve, setShouldObserve] = useState(true);
-  const eventListener = useRef<EmitterSubscription | undefined>(undefined);
-  const xData = useRef({lastX: undefined, xChanged: false});
-  const [direction, setDirection] = useState(undefined);
-  // instead of changed/not changed, try moved left/moved right
+const {Stateful} = Demo;
 
-  useEffect(() => {
-    if (shouldObserve) {
-      console.log('STARTING');
-      const eventEmitter = new NativeEventEmitter(Accelerometer);
-      eventListener.current = eventEmitter.addListener(
-        'Accelerometer',
-        event => {
-          console.log(event);
-          if (event.x === 0) {
-            setDirection('Not moving');
-          } else {
-            setDirection('moving');
-          }
-        },
-      );
-      return () => eventListener.current?.remove();
-    } else if (!shouldObserve) {
-      eventListener.current?.remove();
-    }
-  }, [shouldObserve]);
-
+function App() {
   return (
-    <>
-      <Button title="start events" onPress={() => setShouldObserve(true)} />
-      <Button title="stop events" onPress={() => setShouldObserve(false)} />
-      <Text>X Changing: {direction}</Text>
-    </>
-  );
-}
-
-const App = () => {
-  return (
-    <SafeAreaView>
-      <AccelerometerObserver />
-      <Text>New Architecture</Text>
+    <SafeAreaView style={{height: '100%', width: '100%'}}>
+      <Stateful />
+      <Text>Old Architecture</Text>
     </SafeAreaView>
   );
-};
+}
 
 export default App;
